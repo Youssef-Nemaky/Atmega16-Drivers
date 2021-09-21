a0ce586bc02e91cab933c0c1a66eede57b220232
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-09-21 19:33:52 +0200

    Add timer.c/timer.h files but no code yet

---
+++ MCAL/Timer_Driver/timer.h
@@ -0,0 +1,22 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 20/9/2021
+* Program/Purpose: Timer.h which includes all typedef/enums/structures and function
+  prototypes used in Timer.c
+==============================================================================
+*/
+#ifndef TIMER_H_
+#define TIMER_H_
+
+/*
+==============================================================================
+* Enums
+==============================================================================
+*/
+typedef enum{
+	TIMER_NORMAL_MODE,
+	TIMER_CTC_MODE,
+	TIMER_PWM_MODE,
+} timer_mode_t;
+#endif /* TIMER_H_ */
