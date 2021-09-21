595852d3cd7529b5089bfd4af3446970ee731c57
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-09-21 19:13:10 +0200

    Add std_types.h file but no code yet

---
+++ MCAL/Important_Header_Files/std_types.h
@@ -0,0 +1,15 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 21/9/2021
+* File: micro_config.h
+* Description: A file that contains microcontroller settings like F_CPU and includes
+  libraries used for that microcontroller
+==============================================================================
+*/
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+
+
+#endif /* STD_TYPES_H_ */
