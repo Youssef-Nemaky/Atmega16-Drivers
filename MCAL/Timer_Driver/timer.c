4c1db5f614b182cf0125f13903fa5d46603e9547
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-09-23 17:23:25 +0200

    Timer0 code before testing

--- MCAL/Timer_Driver/timer.c
+++ MCAL/Timer_Driver/timer.c
@@ -7,3 +7,217 @@
 */
 
 #include "timer.h"
+
+/* Callback pointer that will be called when timer ticks the required number of ticks */
+static void (*timer0_callBackPtr)(void) = NULL_PTR;
+/* A variable that counts the number of ticks for the timer */
+static uint8 numberOfTicks = 0;
+/* A variable that contatins the required number of ticks passed from the user in the configuration structure */
+static uint8 requiredNumOfTicks = 0;
+
+void TIMER0_init(const timer0_config * configPtr){
+    /* Set the prescaler by clearing the first 3 bits and then ORing with the timer_prescaler */
+    TCCR0 = (TCCR0 & 0xF8) | (configPtr->timer_prescaler);
+    /* Save the required number of ticks which will be checked in the ISR */
+    requiredNumOfTicks = configPtr->tick;
+    /* Configure the timer mode */
+    switch (configPtr->timerMode){
+    case TIMER_NORMAL_MODE:
+        /* Enable force output compare */
+        SET_BIT(TCCR0,FOC0);
+        /* 
+        Set the timer to work on normal mode(overflow) by
+        1) Clearing WGM00
+        2) Clearing WGM01
+        */
+        CLEAR_BIT(TCCR0,WGM00);
+        CLEAR_BIT(TCCR0,WGM01);
+        /* 
+        Disconnect OC0 pin as the timer is working in overflow mode by
+        1) Clearing COM00
+        2) Clearing COM01
+        */
+        CLEAR_BIT(TCCR0,COM00);
+        CLEAR_BIT(TCCR0,COM01);
+        /* Disable overflow interrupt in case that ticks are zero */
+        if(configPtr->ticks != 0){
+            /* Enable Timer overflow interrupt */
+            SET_BIT(TIMSK, TOIE0);
+        } else {
+            CLEAR_BIT(TIMSK, TOIE0);
+        }
+        /* Disable Timer output compare match match interrupt */
+        CLEAR_BIT(TIMSK, OCIE0);
+        break;   
+    case TIMER_CTC_MODE:
+        /* Enable force output compare */
+        SET_BIT(TCCR0, FOC0);
+        /* 
+        Set the timer to work on normal mode(overflow) by
+        1) Clearing WGM00
+        2) Setting WGM01
+        */
+        CLEAR_BIT(TCCR0, WGM00);
+        SET_BIT(TCCR0,WGM01);
+
+        /* Configure OC0 pin */
+        switch (configPtr->OC0PinMode)
+        {
+        case OC0_DISCONNECTED:
+            /* 
+            Disconnect OC0 by
+            1) Clearing COM00
+            2) Clearing COM01
+            */
+            CLEAR_BIT(TCCR0, COM00);
+            CLEAR_BIT(TCCR0, COM01);
+            break;
+        case OC0_TOGGLE_ON_COMPARE_MATCH:
+            /*
+            Toggle OC0 on compare match by
+            1) Making OC0 (PB3) pin output
+            2) Setting COM00
+            3) Clearing COM01
+            */
+            DDRB |= (1<<PB3);
+            SET_BIT(TCCR0, COM00);
+            CLEAR_BIT(TCCR0, COM01);
+            break;
+        case OC0_CLEAR_ON_COMPARE_MATCH:
+            /*
+            Clear OC0 on compare match by
+            1) Making OC0 (PB3) pin output
+            2) Clearing COM00
+            3) Setting  COM01
+            */
+            DDRB |= (1<<PB3);
+            CLEAR_BIT(TCCR0, COM00);
+            SET_BIT(TCCR0, COM01);
+            break;
+        case OC0_SET_ON_COMPARE_MATCH:
+            /*
+            Set OC0 on compare match by
+            1) Making OC0 (PB3) pin output
+            2) Setting COM00
+            3) Setting COM01
+            */
+            DDRB |= (1<<PB3);
+            SET_BIT(TCCR0, COM00);
+            SET_BIT(TCCR0, COM01);
+            break;
+        default:
+            break;
+        }
+        /* Disable timer output compare match interrupt in case that ticks are zero */
+        if (configPtr->ticks == 0){
+            /* Disable timer output compare match interrupt */
+            SET_BIT(TIMSK, OCIE0);
+        } else {
+            /* Enable timer output compare match interrupt */
+            CLEAR_BIT(TIMSK, OCIE0);
+        }
+        /* Disable timer overflow interrupt  */
+        CLEAR_BIT(TIMSK, TOIE0);
+        break;
+    case TIMER_PWM_MODE:
+        /* Enable force output compare */
+        SET_BIT(TCCR0,FOC0);
+        /* 
+        Set the timer to work on fast PWM mode by
+        1) Setting WGM00
+        2) Setting WGM01
+        */
+        SET_BIT(TCCR0,WGM00);
+        SET_BIT(TCCR0,WGM01);
+        /* Configure OC0 pin */
+        switch (configPtr->OC0PinMode)
+        {
+        case OC0_DISCONNECTED:
+            /* 
+            Disconnect OC0 by
+            1) Clearing COM00
+            2) Clearing COM01
+            */
+            CLEAR_BIT(TCCR0, COM00);
+            CLEAR_BIT(TCCR0, COM01);
+            break;
+        case OC0_CLEAR_ON_COMPARE_MATCH:
+            /*
+            Clear OC0 on compare match (Non-Inverting Mode) by
+            1) Making OC0 (PB3) pin output
+            2) Clearing COM00
+            3) Setting  COM01
+            */
+            DDRB |= (1<<PB3);
+            CLEAR_BIT(TCCR0, COM00);
+            SET_BIT(TCCR0, COM01);
+            break;
+        case OC0_SET_ON_COMPARE_MATCH:
+            /*
+            Set OC0 on compare match (Inverting Mode) by
+            1) Making OC0 (PB3) pin output
+            2) Setting COM00
+            3) Setting COM01
+            */
+            DDRB |= (1<<PB3);
+            SET_BIT(TCCR0, COM00);
+            SET_BIT(TCCR0, COM01);
+            break;
+        default:
+            break;
+        }
+        /* Disable timer output compare match interrupt in case that ticks are zero */
+        if (configPtr->ticks == 0){
+            /* Disable timer output compare match interrupt */
+            SET_BIT(TIMSK, OCIE0);
+        } else {
+            /* Enable timer output compare match interrupt */
+            CLEAR_BIT(TIMSK, OCIE0);
+        }
+        /* Disable timer overflow interrupt  */
+        CLEAR_BIT(TIMSK, TOIE0);
+        break;     
+    default:
+        break;
+    }
+}
+
+
+void TIMER0_setCallBack(void (*ptrToFunction)(void)){
+    timer0_callBackPtr = ptrToFunction;
+}
+
+
+void TIMER0_deInit(void){
+    TCCR0 = 0;
+    CLEAR_BIT(TIMSK, TOIE0);
+    CLEAR_BIT(TIMSK, OCIE0);
+}
+
+void TIMER0_start(const timer0_config * configPtr){
+    TCCR0 = (TCCR0 & 0xF8) | (configPtr->timer_prescaler);
+}
+
+void TIMER0_stop(void){
+    TCCR0 = (TCCR0 & 0xF8) | NO_CLOCK;
+}
+
+ISR(TIMER0_OVF_vect){
+    numberOfTicks++;
+    if(numberOfTicks == requiredNumOfTicks){
+        numberOfTicks = 0;
+        if(timer0_callBackPtr != NULL_PTR){
+            (*timer0_callBackPtr)();
+        }
+    }
+}
+
+ISR(TIMER0_COMP_vect){
+    numberOfTicks++;
+    if(numberOfTicks == requiredNumOfTicks){
+        numberOfTicks = 0;
+        if(timer0_callBackPtr != NULL_PTR){
+            (*timer0_callBackPtr)();
+        }
+    }
+}
--- MCAL/Timer_Driver/timer.h
+++ MCAL/Timer_Driver/timer.h
@@ -11,12 +11,62 @@
 
 /*
 ==============================================================================
-* Enums
+* import micro_config.h, common_macros.h and std_types.h files
 ==============================================================================
 */
+#include "../Important_Header_Files/micro_config.h"
+#include "../Important_Header_Files/common_macros.h"
+#include "../Important_Header_Files/std_types.h"
+
+
+/*
+==============================================================================
+* 								   Types
+==============================================================================
+*/
+
 typedef enum{
 	TIMER_NORMAL_MODE,
-	TIMER_CTC_MODE,
+	TIMER_CTC_MODE = 2,
 	TIMER_PWM_MODE,
 } timer_mode_t;
+
+typedef enum{
+	OC0_DISCONNECTED,
+	OC0_TOGGLE_ON_COMPARE_MATCH,
+	OC0_CLEAR_ON_COMPARE_MATCH,
+	OC0_SET_ON_COMPARE_MATCH,
+} OC0_pin_mode_t;
+
+/* maybe delete me */
+typedef enum{
+	PWM_NON_INVERTING_MODE,
+	PWM_INTERTING_MODE,
+} PWM_MODE_t;
+
+typedef enum{
+	NO_CLOCK,
+	F_CPU_1,
+	F_CPU_8,
+	F_CPU_64,
+	F_CPU_256,
+	F_CPU_1024,
+	EX_CLK_FALLING_EDGE,
+	EX_CLK_RISING_EDGE,
+} timer0_prescaler_t;
+
+typedef struct{
+	timer_mode_t timerMode;
+	timer0_prescaler_t timer_prescaler;
+	OC0_pin_mode_t OC0PinMode; /* depends on the mode */
+	uint8 ticks;
+} timer0_config;
+
+/*
+==============================================================================
+* 							Function Prototypes
+==============================================================================
+*/
+void TIMER0_init(timer0_config * configPtr);
+
 #endif /* TIMER_H_ */
