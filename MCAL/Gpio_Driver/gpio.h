b80dbd136acf76faed147b0449397c9d1708a4b0
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-10 13:34:03 +0200

    Add important header files to GPIO.h

--- MCAL/Gpio_Driver/gpio.h
+++ MCAL/Gpio_Driver/gpio.h
@@ -10,6 +10,15 @@
 #ifndef GPIO_H_
 #define GPIO_H_
 
+/*
+==============================================================================
+* import micro_config.h, common_macros.h and std_types.h files
+==============================================================================
+*/
+#include "../Important_Header_Files/micro_config.h"
+#include "../Important_Header_Files/common_macros.h"
+#include "../Important_Header_Files/std_types.h"
+
 
 
 #endif /* GPIO_H_ */
