440b6439e210b9bb1299ec553ef4dcf701e2deee
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-10 12:58:31 +0200

    Add initial files for GPIO driver

---
+++ MCAL/Gpio_Driver/gpio.h
@@ -0,0 +1,15 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 10/10/2021
+* Program/Purpose: gpio.h which includes all typedef/enums/structures and function
+  prototypes used in gpio.c
+==============================================================================
+*/
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+
+
+#endif /* GPIO_H_ */
