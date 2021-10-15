37f47464f8e439ac1e747bdf11e03b89fd981118
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-15 22:29:33 +0200

    Add initial files for external interrupt driver

---
+++ MCAL/External_Interrupt_Driver/external_interrupt.h
@@ -0,0 +1,14 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 15/10/2021
+* Name: external_interrupt.h
+* Program/Purpose: external_interrupt.h which includes all typedef/enums/structures and function
+  prototypes used in external_interrupt.c
+==============================================================================
+*/
+
+#ifndef EXTERNAL_INTERRUPT_H_
+#define EXTERNAL_INTERRUPT_H_
+
+#endif /* EXTERNAL_INTERRUPT_H_ */
