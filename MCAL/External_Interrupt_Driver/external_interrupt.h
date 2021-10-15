7a91b6acc0b460c922835bc32487f5ba802f34e6
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-15 22:32:53 +0200

    Import header files in external_interrupt.h

--- MCAL/External_Interrupt_Driver/external_interrupt.h
+++ MCAL/External_Interrupt_Driver/external_interrupt.h
@@ -11,4 +11,15 @@
 #ifndef EXTERNAL_INTERRUPT_H_
 #define EXTERNAL_INTERRUPT_H_
 
+/*
+==============================================================================
+* import micro_config.h, common_macros.h and std_types.h files
+==============================================================================
+*/
+#include "../Important_Header_Files/micro_config.h"
+#include "../Important_Header_Files/common_macros.h"
+#include "../Important_Header_Files/std_types.h"
+#include "../Gpio_Driver/gpio.h"
+
+
 #endif /* EXTERNAL_INTERRUPT_H_ */
