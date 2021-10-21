3c6439fabbfa8114247dc8357cef9ca420f4d434
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-21 12:12:24 +0200

    Add initial files for Button_Driver

---
+++ HAL/Button_Driver/button.h
@@ -0,0 +1,16 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 21/10/2021
+* Name: external_interrupt.h
+* Program/Purpose: button.h which includes all typedef/enums/structures and function
+  prototypes used in button.c
+==============================================================================
+*/
+
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+
+
+#endif /* BUTTON_H_ */
