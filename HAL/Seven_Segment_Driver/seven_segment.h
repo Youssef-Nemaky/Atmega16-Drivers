ba5944852eb36a19752c036c81de4610905a22a8
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 16:16:56 +0200

    Add initial files for Seven_Segment_Driver

---
+++ HAL/Seven_Segment_Driver/seven_segment.h
@@ -0,0 +1,12 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 25/10/2021
+* Name: seven_segment.h
+* Program/Purpose: seven_segment.h which includes all typedef/enums/structures and function
+  prototypes used in seven_segment.c
+==============================================================================
+*/
+
+
+
