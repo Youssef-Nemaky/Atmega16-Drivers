1b40997d531914f6d2617a5912fc5c3a3644fe02
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 16:46:40 +0200

    Add functions prototypes

--- HAL/Seven_Segment_Driver/seven_segment.h
+++ HAL/Seven_Segment_Driver/seven_segment.h
@@ -27,4 +27,19 @@ typedef struct{
     GPIO_half_port_number_t portHalf;
 } SEVENSEGMENT_config_t;
 
+/*
+==============================================================================
+*                                Function Prototypes  
+==============================================================================
+*/
 
+/* 
+ * Setup the pins for the seven segment display   
+ */
+void SEVENSEGMENT_init(const SEVENSEGMENT_config_t * configPtr);
+
+/* 
+ * Display a number to the seven-segment 
+ * seven segment uses 4 pins -> 4 bits -> max value: 2^4 - 1 = 0xF (15) 
+ */
+void SEVENSEGMENT_displayNumber(const SEVENSEGMENT_config_t * configPtr, uint8 numToDisplay);
