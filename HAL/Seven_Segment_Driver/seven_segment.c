134775217663bab30220ebc0dda39ecb129bbbbd
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-10-25 16:48:14 +0200

    Add SEVENSEGMENT_init function code

--- HAL/Seven_Segment_Driver/seven_segment.c
+++ HAL/Seven_Segment_Driver/seven_segment.c
@@ -12,3 +12,18 @@
 ==============================================================================
 */
 #include "seven_segment.h"
+
+/*
+==============================================================================
+*                                 Functions  
+==============================================================================
+*/
+
+/* 
+ * Setup the pins for the seven segment display   
+ * if the port number or portHalf are not correct, the request is not handled
+ */
+void SEVENSEGMENT_init(const SEVENSEGMENT_config_t * configPtr){
+    /* Call GPIO init function to set the half of the port direction */
+    GPIO_setHalfPortDirection(configPtr->portNumber, PORT_OUTPUT, configPtr->portHalf);
+}
