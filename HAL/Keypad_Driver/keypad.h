e729769a52c1359a5ab90f4eafbb8d564b6669af
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-11-12 19:59:15 +0200

    Add initial files for keypad driver

---
+++ HAL/Keypad_Driver/keypad.h
@@ -0,0 +1,9 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 12/11/2021
+* Name: keypad.h
+* Program/Purpose: keypad.h which includes all typedef/enums/structures and function
+  prototypes used in keypad.c
+==============================================================================
+*/
