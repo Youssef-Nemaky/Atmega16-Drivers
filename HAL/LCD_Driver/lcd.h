89b5fd4e2a31b29adbbcbd94854361fb290eef1b
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-11-16 20:57:09 +0200

    Include gpio.h to lcd.h driver

---
+++ HAL/LCD_Driver/lcd.h
@@ -0,0 +1,21 @@
+/*
+==============================================================================
+* Authour: Youssef El-Nemaky
+* Date: 16/11/2021
+* Name: lcd.h
+* Program/Purpose: lcd.h which includes all typedef/enums/structures and function
+  prototypes used in lcd.c
+==============================================================================
+*/
+
+#ifndef LCD_H_
+#define LCD_H_
+
+/*
+==============================================================================
+*                       Include gpio.h file
+==============================================================================
+*/
+#include "../../MCAL/Gpio_Driver/gpio.h"
+
+#endif
