a79801793933f1f59ed8ad5c145d4db93076e9ec
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-11-17 23:53:50 +0200

    Add static function LCD_writingSequence code

--- HAL/LCD_Driver/lcd.c
+++ HAL/LCD_Driver/lcd.c
@@ -34,3 +34,40 @@ static void LCD_writingSequence(uint8 a_option);
 *                                  Functions  
 ==============================================================================
 */
+
+/*
+==============================================================================
+*                               Static Functions  
+==============================================================================
+*/
+
+/*
+==============================================================================
+* [Function Name]: LCD_writingSequence
+* [Description]: Used to initiaite the writing sequence of the LCD to send either
+*               a command or a character. 
+* [Args]:
+*   [in]: uint8 : option: either choose between sending a command or a character.
+*   [out]: none
+*   [in/out]: none
+* [Returns]: none
+==============================================================================
+*/
+static void LCD_writingSequence(uint8 a_option){
+    /* Check whether you are sending a command or data */
+    if(a_option == LCD_SENDING_COMMAND){
+        /* Make RS -> LOW to select command register */
+        GPIO_writePin(LCD_CMD_PORT, LCD_RS, LOW);
+    } else {
+        /* Make RS -> HIGH to select data register */
+        GPIO_writePin(LCD_CMD_PORT, LCD_RS, HIGH);
+    }
+    /* Make RW -> LOW to select writing to the LCD */
+    GPIO_writePin(LCD_CMD_PORT, LCD_RW, LOW);
+    
+    /* A high to low pulse on enable pin */
+    GPIO_writePin(LCD_CMD_PORT, LCD_EN, HIGH);
+    _delay_ms(1); /* A delay of 1 ms */
+    GPIO_writePin(LCD_CMD_PORT, LCD_EN, LOW);
+    _delay_ms(2); /* A delay of 2 ms */
+}
