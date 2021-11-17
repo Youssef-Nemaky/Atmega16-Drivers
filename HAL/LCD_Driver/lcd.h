12bc1838c2148d7b6ba5901974cd5c36d59195ab
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-11-17 23:35:20 +0200

    Update definitions to support 4-bit mode

--- HAL/LCD_Driver/lcd.h
+++ HAL/LCD_Driver/lcd.h
@@ -26,21 +26,38 @@
 #define LCD_EIGHT_BIT_MODE 0
 #define LCD_FOUR_BIT_MODE 1
 
-/**** Settings To Change ****/
+#define LCD_SENDING_COMMAND 0
+#define LCD_SENDING_DATA 1
+
+/**** Start of Settings To Change ****/
+
 /* LCD MODE */
 /* Choose between LCD_EIGHT_BIT_MODE or LCD_FOUR_BIT_MODE */
-#define LCD_MODE LCD_EIGHT_BIT_MODE
+#define LCD_MODE LCD_FOUR_BIT_MODE
 
-/* LCD PORT */
+#if (LCD_MODE == LCD_FOUR_BIT_MODE)
+/* Select which half the LCD is connected to
+ * Choose between 
+ * 1) GPIO_FIRST_HALF
+ * or
+ * 2) GPIO_SECOND_HALF
+ */
+#define LCD_PORT_HALF GPIO_SECOND_HALF
+#endif
+
+/* LCD Data Port */
 #define LCD_DATA_PORT PORTA_ID
-#define LCD_CTRL_PORT  PORTB_ID
+/* LCD Control Pins Port */
+#define LCD_CMD_PORT  PORTB_ID
+/* LCD Control Pins */
 #define LCD_RS PB0
 #define LCD_RW PB1
 #define LCD_EN PB2
 
-#if (LCD_MODE == LCD_FOUR_BIT_MODE)
-#define LCD_PORT_HALF GPIO_FIRST_HALF
-#endif
+
+/**** End of Settings To Change ****/
+
+
 /******************* LCD COMMANDS *******************/
 
 #define LCD_CMD_CLEAR_DISP                   (0x01)
@@ -50,7 +67,8 @@
 #define LCD_CMD_DISP_ON_CURSOR_OFF           (0x0C)
 #define LCD_CMD_DISP_ON_CURSOR_ON            (0x0F)
 #define LCD_CMD_DISP_ON_CURSOR_BLINK         (0x0E)
-#define LCD_CMD_TWO_LINES_MODE               (0x38)
+#define LCD_CMD_TWO_LINES_8_BIT_MODE         (0x38)
+#define LCD_CMD_TWO_LINES_4_BIT_MODE         (0x28)
 #define LCD_CMD_FIRST_LINE_ADDRESS           (0x80)
 #define LCD_CMD_SECOND_LINE_ADDRESS          (0xC0)
 #define LCD_CMD_THRID_LINE_ADDRESS           (0x90)
